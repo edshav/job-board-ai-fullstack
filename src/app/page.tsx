47fa92f3aa8d905df2147eda90a3659506f78780
Eduard Shavyrko <edshav@protonmail.com>
2025-06-19 21:16:28 -0300

    feat: add Sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { AppSidebarClient } from './_AppSidebarClient';
+
 export default function HomePage() {
-  return <h1>Hi</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Some Action</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">asafasfs</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
